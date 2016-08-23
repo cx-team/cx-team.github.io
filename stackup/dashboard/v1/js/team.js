0fba2256d19f555554e6099d5718dfd0d17506c9
Hung Nguyen <hung.nguyen@cogini.com>
2016-08-23 13:09:34 +0700

    fix team page

--- stackup/dashboard/v1/js/team.js
+++ stackup/dashboard/v1/js/team.js
@@ -1,29 +1,43 @@
 $.ajax({
     type: 'GET',
 	dataType: 'json',
-    url: api_host + '/admin/dashboard/' + approval_id + '/temp_teams',
+    url: api_host + '/admin/dashboard/' + season_id + '/temp_teams',
     success: function (result) {
         console.log(result);
         result.forEach(function(team){
+
+            var inputs = team.detail.inputs;
+            if ($.isEmptyObject(inputs)) {
+                inputs = {
+                    name: '',
+                    name_alt: '',
+                    name_abbrv: '',
+                    city: '',
+                    country: '',
+                    division: '',
+                    uniqueid: '',
+                }
+            }
+            
             $('#js-team-list').append(
                 '<tr id="js-team' + team.id + '">' +
-                '<td>' + team.detail.inputs.name +'</td>' +
-                '<td>' + team.detail.inputs.name_alt +'</td>' +
-                '<td>' + team.detail.inputs.name_abbrv +'</td>' +
-                '<td>' + team.detail.inputs.city +'</td>' +
-                '<td>' + team.detail.inputs.country +'</td>' +
-                '<td>' + team.detail.inputs.division +'</td>' +
-                '<td>' + team.detail.inputs.uniqueid +'</td>' +
+                '<td>' + (inputs.hasOwnProperty("name") ? inputs.name : '') + '</td>' +
+                '<td>' + (inputs.hasOwnProperty("name_alt") ? inputs.name_alt : '') + '</td>' +
+                '<td>' + (inputs.hasOwnProperty("name_abbrv") ? inputs.name_abbrv : '') + '</td>' +
+                '<td>' + (inputs.hasOwnProperty("city") ? inputs.city : '') + '</td>' +
+                '<td>' + (inputs.hasOwnProperty("country") ? inputs.country : '') + '</td>' +
+                '<td>' + (inputs.hasOwnProperty("division") ? inputs.division : '') + '</td>' +
+                '<td>' + (inputs.hasOwnProperty("uniqueid") ? inputs.uniqueid : '') + '</td>' +
                 '<td><button class="js-edit btn-xs btn-primary btn-fill" data-id="' + team.id + '">EDIT</button></td>' +
                 '</tr>' +
                 '<tr id="js-team-form' + team.id + '" style="display: none">' +
-                '<td><input id="js-team-name' + team.id + '" type="text" name="name" value="' + team.detail.inputs.name + '"><br></td>' +
-                '<td><input id="js-team-name-alt' + team.id + '" type="text" name="name_alt" value="' + team.detail.inputs.name_alt + '"><br></td>' +
-                '<td><input id="js-team-name-abbrv' + team.id + '" type="text" name="name_abbrv" value="' + team.detail.inputs.name_abbrv + '"><br></td>' +
-                '<td><input id="js-team-city' + team.id + '" type="text" name="city" value="' + team.detail.inputs.city + '"><br></td>' +
-                '<td><input id="js-team-country' + team.id + '" type="text" name="country" value="' + team.detail.inputs.country + '"><br></td>' +
-                '<td><input id="js-team-division' + team.id + '" type="text" name="division" value="' + team.detail.inputs.division + '"><br></td>' +
-                '<td><input id="js-team-uniqueid' + team.id + '" type="text" name="uniqueid" value="' + team.detail.inputs.uniqueid + '"><br></td>' +
+                '<td><input id="js-team-name' + team.id + '" type="text" name="name" value="' + (inputs.hasOwnProperty("name") ? inputs.name : '') + '"><br></td>' +
+                '<td><input id="js-team-name-alt' + team.id + '" type="text" name="name_alt" value="' + (inputs.hasOwnProperty("name_alt") ? inputs.name : '') + '"><br></td>' +
+                '<td><input id="js-team-name-abbrv' + team.id + '" type="text" name="name_abbrv" value="' + (inputs.hasOwnProperty("name_abbrv") ? inputs.name : '') + '"><br></td>' +
+                '<td><input id="js-team-city' + team.id + '" type="text" name="city" value="' + (inputs.hasOwnProperty("city") ? inputs.name : '') + '"><br></td>' +
+                '<td><input id="js-team-country' + team.id + '" type="text" name="country" value="' + (inputs.hasOwnProperty("country") ? inputs.name : '') + '"><br></td>' +
+                '<td><input id="js-team-division' + team.id + '" type="text" name="division" value="' + (inputs.hasOwnProperty("division") ? inputs.name : '') + '"><br></td>' +
+                '<td><input id="js-team-uniqueid' + team.id + '" type="text" name="uniqueid" value="' + (inputs.hasOwnProperty("uniqueid") ? inputs.name : '') + '"><br></td>' +
                 '<td><button class="js-update btn-xs btn-success btn-fill" data-id="' + team.id + '">SAVE</button></td>' +
                 '</tr>'
             );
@@ -63,7 +77,7 @@ $(document).on('click', '.js-update', function (e) {
 
     $.ajax({
         type: 'PUT',
-        url: api_host + '/admin/dashboard/' + approval_id + '/temp_teams/' + id,
+        url: api_host + '/admin/dashboard/' + season_id + '/temp_teams/' + id,
         data: data,
         success: function (team) {
             console.log(team);
@@ -108,7 +122,7 @@ $(document).on('click', '#js-create', function (e) {
 
     $.ajax({
         type: 'POST',
-        url: api_host + '/admin/dashboard/' + approval_id + '/temp_teams',
+        url: api_host + '/admin/dashboard/' + season_id + '/temp_teams',
         data: data,
         success: function (team) {
             console.log(team);
